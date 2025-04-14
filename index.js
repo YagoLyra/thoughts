ce832903d3b1d09ef8d744cb9bff3594284581a1
Yago Lyra <yagosouzalyra@outlook.com>
2025-04-14 17:34:52 -0300

    Implementação de configurações iniciais do servidor e gerenciamento de sessões
    - Configurado o servidor Express com suporte a Handlebars como template engine.
    - Adicionado middleware para parsing de JSON e dados de formulários.
    - Configurado middleware `express-session` com `session-file-store` para gerenciamento de sessões.
    - Definidas opções de cookie, incluindo `secure`, `maxAge`, `expires` e `httpOnly`.
    - Integrado `express-flash` para suporte a mensagens flash.
    - Criado middleware para disponibilizar dados da sessão em `res.locals` para as views.
    - Configurado diretório público para arquivos estáticos.
    - Estabelecida conexão com o banco de dados utilizando Sequelize.
    - Configurada sincronização do banco de dados e inicialização do servidor na porta 3000.

---
+++ index.js
@@ -0,0 +1,60 @@
+const express = require("express");
+const exphbs = require("express-handlebars");
+const session = require("express-session");
+const FileStore = require("session-file-store")(session);
+const flash = require("express-flash");
+
+const app = express();
+
+const conn = require("./db/conn");
+
+const Thought = require("./models/Thought");
+const User = require("./models/User");
+
+app.engine("handlebars", exphbs.engine());
+app.set("view engine", "handlebars");
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static("public"));
+
+app.use(
+  session({
+    name: "session",
+    secret: "nosso_secret",
+    resave: false,
+    saveUninitialized: false,
+    store: new FileStore({
+      logFn: function () {},
+      path: require("path").join(require("os").tmpdir(), "sessions"),
+    }),
+    cookie: {
+      secure: false,
+      maxAge: 360000,
+      expires: new Date(Date.now() + 360000),
+      httpOnly: true,
+    },
+  })
+);
+
+app.use(flash());
+
+app.use((req, res, next) => {
+  if (req.session.userid) {
+    res.locals.session = req.session;
+  }
+
+  next();
+});
+
+conn
+  .sync({ force: false })
+  .then(() => {
+    app.listen(3000, () => {
+      console.log("Servidor rodando na porta 3000!");
+    });
+  })
+  .catch((err) => {
+    console.error("Erro ao conectar ao banco de dados:", err);
+  });
