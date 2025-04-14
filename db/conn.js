5eb10d9d4cf1b1b9a3888150a84126c001d63824
Yago Lyra <yagosouzalyra@outlook.com>
2025-04-14 17:35:49 -0300

    Configuração da conexão com o banco de dados MySQL
    - Configurado Sequelize para conexão com o banco de dados MySQL utilizando as credenciais:
      - Banco de dados: "thoughts"
      - Usuário: "root"
      - Senha: ""
      - Host: "localhost"
      - Dialeto: "mysql"
    - Adicionado método `authenticate` para verificar a conexão com o banco de dados.
    - Incluída mensagem de sucesso ou erro no console para feedback da conexão.
    - Exportado o objeto Sequelize para uso em outras partes da aplicação.

---
+++ db/conn.js
@@ -0,0 +1,15 @@
+const { Sequelize } = require("sequelize");
+
+const sequelize = new Sequelize("thoughts", "root", "", {
+  host: "localhost",
+  dialect: "mysql",
+});
+
+try {
+  sequelize.authenticate();
+  console.log("Conexão com o banco de dados realizada com sucesso!");
+} catch (error) {
+  console.error("Não foi possível conectar ao banco de dados:", error);
+}
+
+module.exports = sequelize;
