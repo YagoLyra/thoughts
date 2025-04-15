814d0c3cb079409b038c3f9ca8057e18cbcb2df1
Yago Lyra <yagosouzalyra@outlook.com>
2025-04-15 12:06:12 -0300

    Adicionadas rotas para exibição de pensamentos e dashboard
    - Configurada rota GET `/dashboard` protegida pelo middleware `checkAuth`, utilizando o método `dashboard` do `ThoughtController`.
    - Configurada rota GET `/` para exibir pensamentos públicos utilizando o método `showThoughts` do `ThoughtController`.

--- routes/thoughtsRoutes.js
+++ routes/thoughtsRoutes.js
@@ -2,6 +2,9 @@ const express = require("express");
 const router = express.Router();
 const ThoughtController = require("../controllers/ThoughtController");
 
+const checkAuth = require("../helpers/auth").checkAuth;
+
+router.get("/dashboard", checkAuth, ThoughtController.dashboard);
 router.get("/", ThoughtController.showThoughts);
 
 module.exports = router;
