5a026d51ea7c320c5b066d9cae8b1cac17929f42
Yago Lyra <yagosouzalyra@outlook.com>
2025-04-15 12:03:22 -0300

    Ajustes no processo de login de usuários
    - Adicionada validação para verificar se os campos de e-mail e senha estão preenchidos.
    - Implementada verificação de existência do usuário no banco de dados.
    - Adicionada validação de senha utilizando `bcrypt.compareSync`.
    - Configurada exibição de mensagens de erro com `express-flash` para casos de:
      - Campos não preenchidos.
      - Usuário não encontrado.
      - Senha inválida.
    - Configurado redirecionamento para a página inicial após login bem-sucedido.

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -6,6 +6,40 @@ module.exports = class AuthController {
     res.render("auth/login");
   }
 
+  static async loginPost(req, res) {
+    const { email, password } = req.body;
+
+    if (!email || !password) {
+      req.flash("message", "Por favor, preencha todos os campos!");
+      res.render("auth/login");
+
+      return;
+    }
+
+    const user = await User.findOne({ where: { email: email } });
+
+    if (!user) {
+      req.flash("message", "Usuário não encontrado!");
+      res.render("auth/login");
+
+      return;
+    }
+
+    const passwordMatch = bcrypt.compareSync(password, user.password);
+    if (!passwordMatch) {
+      req.flash("message", "Senha inválida!");
+      res.render("auth/login");
+
+      return;
+    }
+
+    req.session.userid = user.id;
+    req.flash("message", "Login realizado com sucesso!");
+    req.session.save(() => {
+      res.redirect("/");
+    });
+  }
+
   static register(req, res) {
     res.render("auth/register");
   }
